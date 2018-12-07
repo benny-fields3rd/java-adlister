c11d1a7c96a4a3b0146a66b5fecbe01e2daf398a
benny-fields3rd <benny.fields3rd@gmail.com>
2018-12-07 15:44:22 -0600

    created and completed servlet exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @Override
+    protected void doGet( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
+        System.out.println("You will see this in the console");
+//        resp.getWriter().println("<h1>Hello World!</h1>");
 
+        PrintWriter out = resp.getWriter();
+
+        String name = req.getParameter("name");
+
+        if (name == null) {
+            name = "World";
+        }
+            out.printf("<h1>Hello %s!</h1>", name);
     }
 }
